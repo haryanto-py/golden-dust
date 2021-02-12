576ec3d54b1c8133d2ecc2d7deda65fba0005657
haryanto-py <68938263+haryanto-py@users.noreply.github.com>
2021-02-12 23:21:22 +0700

    Add files via upload

---
+++ bus.py
@@ -0,0 +1,41 @@
+class Bus:
+
+    __jumlah = 0
+
+    def __init__(self, supir, trayek, kas, penumpang):
+        self.supir = supir
+        self.trayek = trayek
+        self.kas = kas
+        self.penumpang = penumpang
+        Bus.__jumlah += 1
+    
+    def penumpangNaik(self, nama):
+        self.penumpang.append(nama)
+        return self.penumpang
+    
+    def penumpangTurun(self, nama, bayar):
+        if len(self.penumpang) == 0:
+            print("bus kosong!")
+            return False
+
+        for i in self.penumpang:
+            if i == nama:
+                self.penumpang.remove(nama)
+                self.kas += bayar
+                return self.penumpang
+    
+    @classmethod
+    def getJumlah(cls):
+        return cls.__jumlah
+
+    @staticmethod
+    def getJumlah2():
+        return Bus.__jumlah
+
+bus1 = Bus("Haryanto", "Caringin - Sd. Serang", 0, [])
+
+bus1.penumpangNaik("Haryanto")
+bus1.penumpangNaik("Hidayat")
+
+print(Bus.getJumlah())
+print(Bus.getJumlah2())
